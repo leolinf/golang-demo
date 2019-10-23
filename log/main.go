fe460664a0f268567c64ca40b01e4aa83cdab90f
leolinf <funlin.py@foxmail.com>
2019-10-23 18:00:30 +0800

    添加一个日志管理器

---
+++ log/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "golang-demo/log/logger"
+
+func main() {
+
+	// 设置是从控制台输出还是日志文件
+	logger.SetConsole(true)
+	// 设置日志存放路径
+	logger.SetRollingDaily("/Users/fanlin/Golang/src/golang-demo/log", "test.log", "test.error.log")
+	// 设置日志级别
+	loglevel, _ := logger.LoggerLevelIndex("INFO")
+	logger.SetLevel(loglevel)
+
+	logger.Debugf("Debug")
+	logger.Infof("Info")
+	logger.Warnf("Warn")
+	logger.Errorf("Error")
+	logger.Fatalf("Fatal")
+	logger.Infof("Infof")
+}
